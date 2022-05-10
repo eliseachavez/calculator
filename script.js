9323e11c21b7b7fe051e0fa16b6ab9f4a4f4cdee
lemur <noctiphobic@gmail.com>
2022-05-10 15:11:40 -0500

    Fix undefined error with operate function: needed to return the returned value

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Create basic math operations: add, subtract, multiply, divide and test in browser
-let a = 1;
-let b = 1;
+var a = 1;
+var b = 1;
 function add(a,b){
     return a + b;
 }
@@ -19,21 +19,18 @@ function div(a,b) {
 
 function operate(op, a,b) {
 // don't need input validation if user restricted to what's on the buttons
-let opList = ['+', '-', '*', '/'];
-    for (op in opList) {
-        if ( op === '+') {
-            add(a,b);
-        } else if ( op === '-') {
-            sub(a,b);
-        } else if ( op === '*') {
-            mult(a,b);
-        } else if ( op === '/') {
-            div(a,b);
-        }
+    if ( op === '+') {
+        return add(a,b);
+    } else if ( op === '-') {
+        return sub(a,b);
+    } else if ( op === '*') {
+        return mult(a,b);
+    } else if ( op === '/') {
+        return div(a,b);
     }
 }
 
-console.log(add(a,b));
-console.log(sub(a,b));
-console.log(mult(a,b));
-console.log(div(a,b));
+console.log(operate('+',a,b));
+console.log(operate('-',a,b));
+console.log(operate('*',a,b));
+console.log(operate('/',a,b));
