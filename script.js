aeee60f8e172aafce486e725cded333b625d5439
lemur <noctiphobic@gmail.com>
2022-05-10 06:34:40 -0500

    Write operator function (add, sub, mult, div)

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
 //Create basic math operations: add, subtract, multiply, divide and test in browser
-//ADD
-function add((a, b) => return a + b );
+let a = 1;
+let b = 1;
+function add(a,b){
+    return a + b;
+}
+
+function sub(a,b) {
+    return a - b;
+}
+
+function mult(a,b) {
+    return a * b;
+}
+
+function div(a,b) {
+    return a / b;
+}
+
+function operate(op, a,b) {
+// don't need input validation if user restricted to what's on the buttons
+let opList = ['+', '-', '*', '/'];
+    for (op in opList) {
+        if ( op === '+') {
+            add(a,b);
+        } else if ( op === '-') {
+            sub(a,b);
+        } else if ( op === '*') {
+            mult(a,b);
+        } else if ( op === '/') {
+            div(a,b);
+        }
+    }
+}
+
+console.log(add(a,b));
+console.log(sub(a,b));
+console.log(mult(a,b));
+console.log(div(a,b));
